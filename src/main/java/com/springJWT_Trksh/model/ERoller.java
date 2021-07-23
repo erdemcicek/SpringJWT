20093c4795a2f0ccf1321aeb9c12cd34e1fdc344
erdemcicek <erdem25123@gmail>
2021-07-23 21:31:05 +0200

    first commit JWT

---
+++ src/main/java/com/springJWT_Trksh/model/ERoller.java
@@ -0,0 +1,5 @@
+package com.springJWT_Trksh.model;
+
+public enum ERoller {
+
+}
---
+++ src/test/java/com/springJWT_Trksh/SpringJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springJWT_Trksh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
